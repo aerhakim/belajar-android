82afdf141f2df3a6f2b3ff834bc68a13be9aed53
AR@hakim69 <arhakim@students.amikom.ac.id>
2021-03-16 16:22:50 +0700

    Mbuh porah 1

--- app/src/main/java/com/sendData/Receive.kt
+++ app/src/main/java/com/sendData/Receive.kt
@@ -2,6 +2,7 @@ package com.sendData
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.arhakim.materifintech.R
 
 class Receive : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/sendData/Send.kt
+++ app/src/main/java/com/sendData/Send.kt
@@ -2,6 +2,7 @@ package com.sendData
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.arhakim.materifintech.R
 
 class Send : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
