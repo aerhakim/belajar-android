ed8b4cb3745d79d1640d5e3b040eea77bee7d8f3
AR@hakim69 <arhakim@students.amikom.ac.id>
2021-03-16 17:07:53 +0700

    Update 1.4: Fix DataActivity dan MainActivity

--- app/src/main/java/com/arhakim/materifintech/DataActivity.kt
+++ app/src/main/java/com/arhakim/materifintech/DataActivity.kt
@@ -3,7 +3,7 @@ package com.arhakim.materifintech
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DataActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,3 +13,4 @@ class MainActivity : AppCompatActivity() {
         val nama = intent.getStringArrayExtra("nama", "")
     }
 }
+
--- app/src/main/java/com/arhakim/materifintech/MainActivity.kt
+++ app/src/main/java/com/arhakim/materifintech/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.arhakim.materifintech
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonSubmit: Button
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        buttonSubmit = findViewById(R.id.btn_submit)
+
+        buttonSubmit.setOnClickListener {
+            val nama = edtEmail.text.toString()
+            Toast.makeText(this, nama, Toast.LENGTH_SHORT).show()
+
+            val intentGotoData = Intent(this, DataActivity::class.java).apply {
+//                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+            startActivity(intentGotoData)
+        }
     }
+
 }
